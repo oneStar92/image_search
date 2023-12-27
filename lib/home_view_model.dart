56b8157ecf0af798f65e06ebc2ded1221fc0e8a6
Ayaan <onestar7984@gmail.com>
2023-12-27 22:02:29 +0900

    refactor: HomeViewModel 기능 구현

--- lib/home_view_model.dart
+++ lib/home_view_model.dart
@@ -1,11 +1,15 @@
 import 'dart:async';
 
+import 'package:image_search/Model/image_model.dart';
+import 'package:image_search/mapper/image_model_mapper.dart';
+import 'package:image_search/repository/repository.dart';
+
 final class HomeViewModel {
-  final _searchResult = StreamController<String>.broadcast();
+  final Repository _searchRepository = DefaultRepository();
+  List<ImageModel> images = [];
 
-  Stream<String> get result => _searchResult.stream;
-
-  void search(String target) {
-
+  Future<void> search(String word) async {
+    final imagesDTO = await _searchRepository.search(word);
+    images = imagesDTO.toModels();
   }
 }
