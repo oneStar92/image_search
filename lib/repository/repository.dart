61fe9a546530fd9f76973d918657edbb85bf096f
Ayaan <onestar7984@gmail.com>
2023-12-27 22:02:06 +0900

    refactor: Repository 기능 구현

--- lib/repository/repository.dart
+++ lib/repository/repository.dart
@@ -1,12 +1,17 @@
+import 'dart:convert';
+
 import 'package:http/http.dart' as http;
+import 'package:image_search/dto/image_dto.dart';
 
 abstract interface class Repository {
-  Future<void> search(String word);
+  Future<ImagesDTO> search(String word);
 }
 
 class DefaultRepository implements Repository {
   @override
-  Future<void> search(String word) async {
-    await http.get(Uri.parse('https://pixabay.com/api/?key=10711147-dc41758b93b263957026bdadb&q=$word&image_type=photo'));
+  Future<ImagesDTO> search(String word) async {
+    final response = await http
+        .get(Uri.parse('https://pixabay.com/api/?key=10711147-dc41758b93b263957026bdadb&q=$word&image_type=photo'));
+    return ImagesDTO.fromJson(jsonDecode(response.body));
   }
 }
